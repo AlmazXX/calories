ab51921658e33d7391f539562d1c0dc3b437492c
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-11 22:13:28 +0600

    Added meal prop to Meal comp Added capitalize func to Meal comp

--- src/components/Meal/MealItem.tsx
+++ src/components/Meal/MealItem.tsx
@@ -1,15 +1,22 @@
+import { FC } from "react";
 import { Link } from "react-router-dom";
+import { capitalize } from "../../helpers";
+import { ApiMeal } from "../../types";
 
-const MealItem = () => {
+interface Props {
+  meal: ApiMeal;
+}
+
+const MealItem: FC<Props> = ({meal}) => {
   return (
     <div className="col-12">
       <div className="card">
         <div className="card-body">
-          <p className="small float-sm-end m-0 text-muted">Meal time</p>
-          <p className="card-title">Description</p>
-          <p className="card-text">Amount of kcal</p>
+          <p className="small float-sm-end m-0 text-muted">{capitalize(meal.mealtime)}</p>
+          <p className="card-title">{meal.description}</p>
+          <p className="card-text"><strong>{meal.calories}</strong> kcal</p>
           <div className="d-flex gap-3 px-0 mb-3">
-            <Link to={`/quotes/edit`} className="btn btn-primary">
+            <Link to={`/meal/${meal.id}/edit`} className="btn btn-primary">
               Edit
             </Link>
             <button className="btn btn-danger">Delete</button>
