f8329117b1c43983b48d9633be0c82b63356b6d1
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-11 20:59:24 +0600

    Created MealFormWrapper for MealForm

---
+++ src/containers/Forms/MealFormWrapper.tsx
@@ -0,0 +1,11 @@
+import MealForm from "../../components/MealForm/MealForm";
+
+const MealFormWrapper = () => {
+  return (
+    <div className='row mt-3'>
+      <MealForm/>
+    </div>
+  );
+};
+
+export default MealFormWrapper;
