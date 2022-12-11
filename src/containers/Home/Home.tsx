e6a946b051b1cea810b3eb15dece40e346e7a158
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-11 21:09:31 +0600

    Made 2 rows in Home cont 1 for Total calories amount and Add meal button 2 for the list of meal consumed

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div className="row mt-3">Home</div>;
+  return (
+    <>
+      <div className="row mt-3">
+        <div className="col-8">Total calories today: <strong></strong></div>
+        <div className="col-4">
+          <Link to="meal/add" className="btn btn-primary">Add new meal</Link>
+        </div>
+      </div>
+      <div className="row mt-3"></div>
+    </>
+  );
 };
 
 export default Home;
