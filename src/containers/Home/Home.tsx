b4a881effe006cf51968cbd8b09a5f8a964a165e
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-11 21:47:14 +0600

    Added Meals comp to Home cont

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import Meals from "../../components/Meal/Meals";
 
 const Home = () => {
   return (
     <>
       <div className="row mt-3">
         <div className="col-8">Total calories today: <strong></strong></div>
-        <div className="col-4">
+        <div className="col-4 text-end">
           <Link to="meal/add" className="btn btn-primary">Add new meal</Link>
         </div>
       </div>
-      <div className="row mt-3"></div>
+      <div className="row mt-3"><Meals/></div>
     </>
   );
 };
