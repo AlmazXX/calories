b5abbac62265e26a05a561b73884eb12c31daa24
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-11 20:23:08 +0600

    Created Navbar comp

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="navbar navbar-expand-sm navbar-dark bg-primary">
+      <div className="container">
+        <span className="navbar-brand">Calories</span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
