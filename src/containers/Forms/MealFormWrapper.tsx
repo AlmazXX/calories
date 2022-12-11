82402ba2860e7b2a7acc4f86288f49e14a212ef2
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-11 21:06:00 +0600

    Placed MealForm in div.col-6 to restrict its width

--- src/containers/Forms/MealFormWrapper.tsx
+++ src/containers/Forms/MealFormWrapper.tsx
@@ -2,8 +2,10 @@ import MealForm from "../../components/MealForm/MealForm";
 
 const MealFormWrapper = () => {
   return (
-    <div className='row mt-3'>
-      <MealForm/>
+    <div className="row mt-3">
+      <div className="col-6">
+        <MealForm />
+      </div>
     </div>
   );
 };
