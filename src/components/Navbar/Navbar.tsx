63f9f8979a48e31fc6ec902046029338eec01ff9
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-13 00:53:56 +0600

    Added link to the main page to Logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className="navbar navbar-expand-sm navbar-dark bg-primary">
       <div className="container">
-        <span className="navbar-brand">Calories</span>
+        <span className="navbar-brand">
+          <Link to="/" className="text-white text-decoration-none">Calories</Link>
+        </span>
       </div>
     </div>
   );
