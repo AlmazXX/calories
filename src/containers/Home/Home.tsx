3e91c39da5bcf80fe891ffcfe4ef25f9cfff0e2a
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-13 01:02:28 +0600

    Added logic to count total calories amount

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -7,23 +7,31 @@ import { ApiMeal, ApiMealsList } from "../../types";
 
 const Home = () => {
   const [meals, setMeals] = useState<ApiMeal[]>([]);
+  const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(false);
   const [deleting, setDeleting] = useState(false);
 
-  const getMeals = useCallback( async() => {
+  const getMeals = useCallback(async () => {
     try {
       setLoading(true);
-      const {data} = await axiosApi.get<ApiMealsList | null>('/meals.json');
-      const newMeals = data ? Object.keys(data).map(id => ({...data[id], id})) : [];
+      const { data } = await axiosApi.get<ApiMealsList | null>("/meals.json");
+      const newMeals = data
+        ? Object.keys(data).map((id) => ({ ...data[id], calories: parseInt(data[id].calories), id }))
+        : [];
       setMeals(newMeals);
-    } finally{
-      setLoading(false)
+    } finally {
+      setLoading(false);
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     void getMeals();
-  }, [getMeals])
+  }, [getMeals]);
+
+
+  useEffect(() => {
+    setTotal(meals.reduce((acc, meal) => acc + meal.calories, 0))
+  }, [meals]);
 
   const deleteMeal = async (id: string) => {
     try {
@@ -33,17 +41,30 @@ const Home = () => {
     } finally {
       setDeleting(false);
     }
-  }
+  };
 
   return (
     <>
       <div className="row mt-3">
-        <div className="col-8">Total calories today: <strong></strong></div>
+        <h4>Meals list</h4>
+      </div>
+      <div className="row mt-3">
+        <div className="col-8">
+          Total calories today: <strong>{total}</strong>
+        </div>
         <div className="col-4 text-end">
-          <Link to="meal/add" className="btn btn-primary">Add new meal</Link>
+          <Link to="meal/add" className="btn btn-primary">
+            Add new meal
+          </Link>
         </div>
       </div>
-      <div className="row mt-3 gap-3">{loading ? <Spinner/> : <Meals meals={meals} isDeleting={deleting} onDelete={deleteMeal}/>}</div>
+      <div className="row mt-3 gap-3">
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Meals meals={meals} isDeleting={deleting} onDelete={deleteMeal} />
+        )}
+      </div>
     </>
   );
 };
