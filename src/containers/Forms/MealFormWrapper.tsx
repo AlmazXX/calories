f91f9f9f15e850c961688c52bbb2ded377482d93
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-13 00:58:17 +0600

    Added logic to edit existing meal Added editMeal func to edit existing meal Added getOneMeal func to receive values of exiting meal Added meal state to store initial values of existing meal Added Spinner to display while loading existing meal

--- src/containers/Forms/MealFormWrapper.tsx
+++ src/containers/Forms/MealFormWrapper.tsx
@@ -1,29 +1,74 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import axiosApi from "../../axiosApi";
 import MealForm from "../../components/MealForm/MealForm";
+import Spinner from "../../components/Spinner/Spinner";
 import { Meal } from "../../types";
 
 const MealFormWrapper = () => {
   const navigate = useNavigate();
+  const { id } = useParams();
+  const [meal, setMeal] = useState<Meal | null>(null);
+  const [saving, setSaving] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const addMeal = async (meal: Meal) => {
     try {
-      setLoading(true);
+      setSaving(true);
       await axiosApi.post("/meals.json", meal);
-      navigate('/');
+      navigate("/");
     } finally {
-      setLoading(false);
+      setSaving(false);
     }
   };
 
+  const editMeal = async (meal: Meal) => {
+    try {
+      setSaving(true);
+      await axiosApi.put(`/meals/${id}.json`, meal);
+    } finally {
+      setSaving(false);
+    }
+  };
+
+  const getOneMeal = useCallback(async () => {
+    try {
+      setLoading(true);
+      const { data } = await axiosApi.get<Meal | null>(`/meals/${id}.json`);
+      const existingMeal = data ? data : null;
+      setMeal(existingMeal);
+    } finally {
+      setLoading(false);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (id) {
+      void getOneMeal();
+    }
+  }, [id, getOneMeal]);
+
   return (
-    <div className="row mt-3">
-      <div className="col-6">
-        <MealForm onSubmit={addMeal} isLoading={loading} />
+    <>
+      <div className="row mt-3">
+        <h4>{!id ? "Add meal" : "Edit Meal"}</h4>
       </div>
-    </div>
+      <div className="row mt-3">
+        <div className="col-6">
+          {loading ? (
+            <Spinner />
+          ) : meal ? (
+            <MealForm
+              existingMeal={meal}
+              isSaving={saving}
+              onSubmit={!id ? addMeal : editMeal}
+            />
+          ) : (
+            <MealForm isSaving={saving} onSubmit={!id ? addMeal : editMeal} />
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
