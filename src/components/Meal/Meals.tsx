0d67c08b40a7c903138ecb2ce437354bb1683d32
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-13 16:25:55 +0600

    Change isDeleting props to idForDelete Passed the props to MealItem with comparing it ro current meal's id

--- src/components/Meal/Meals.tsx
+++ src/components/Meal/Meals.tsx
@@ -4,15 +4,22 @@ import MealItem from "./MealItem";
 
 interface Props {
   meals: ApiMeal[];
-  isDeleting: boolean;
+  idForDelete: string | null;
   onDelete: (id: string) => void;
 }
 
-const Meals: FC<Props> = ({ meals, isDeleting, onDelete }) => {
+const Meals: FC<Props> = ({ meals, idForDelete, onDelete }) => {
   return (
     <>
       {meals.length ? (
-        meals.map((meal) => <MealItem key={meal.id} meal={meal} isDeleting={isDeleting} onClick={() => {onDelete(meal.id)}} />)
+        meals.map((meal) => (
+          <MealItem
+            key={meal.id}
+            meal={meal}
+            isDeleting={idForDelete === meal.id}
+            onClick={() => onDelete(meal.id)}
+          />
+        ))
       ) : (
         <p>No meals added</p>
       )}
