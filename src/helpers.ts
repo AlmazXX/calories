faf3646e99bd810710d398323b776b0565ec333f
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-12-14 16:53:16 +0600

    Updated convertTime, convertDate funcs

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,18 @@
 export const capitalize = (str: string) => {
-  return str[0].toUpperCase() + str.slice(1)
-}
+  return str[0].toUpperCase() + str.slice(1);
+};
+
+export const convertTime = (date: Date) => {
+  return new Date(date).toLocaleString("en-en", {
+    hour: "numeric",
+    minute: "numeric",
+  });
+};
+
+export const convertDate = (date: Date) => {
+  return new Date(date).toLocaleString("en-en", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+};
